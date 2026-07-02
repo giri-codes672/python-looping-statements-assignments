3a34729ff454a76e9340d3236c040e5edc1476ac
giri-codes672 <giribabu9059@gmail.com>
2026-07-02 12:22:14 +0530

    Implement second largest number finder

---
+++ secondlargest.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+if (a > b and a < c) or (a > c and a < b):
+    print(a)
+elif (b > a and b < c) or (b > c and b < a):
+    print(b)
+else:
+    print(c)
